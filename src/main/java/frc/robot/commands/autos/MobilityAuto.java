55156ec7a2892c4b55d43bf1a4e42d50b9a36337
EchoPrograms <brogan.oberhaus30@gmail.com>
2024-09-05 13:16:26 -0400

    comment added for auto description - B

--- src/main/java/frc/robot/commands/autos/MobilityAuto.java
+++ src/main/java/frc/robot/commands/autos/MobilityAuto.java
@@ -6,7 +6,8 @@ import frc.robot.commands.MoveForSeconds;
 import frc.robot.commands.TurnDegrees;
 
 public class MobilityAuto extends SequentialCommandGroup{
-    // TODO: needs to be properly configured to go the correct distance (currently guesses)
+    // Should be ran facing mid-field, placed to the right of the stage (source side).
+    // Just drives straight for 1.5 seconds at max speed (~5 m/s)
     public MobilityAuto(){
         super(new MoveForSeconds(0, 1, 1.5));
     }
